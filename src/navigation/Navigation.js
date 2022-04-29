7f9bb55c7c0750383f936a499bdf1c15eb7c126f
NightDreams <yayodcv@gmail.com>
2022-04-29 18:49:32 -0500

    c3 - Tab navigation set icons

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { Image } from "react-native";
 
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+// import Icon from "react-native-vector-icons/FontAwesome5";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 //screens
 import Favorite from "../screens/Favorite";
@@ -12,9 +15,43 @@ const Tab = createBottomTabNavigator();
 export default function Navigation() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Podex" component={Pokedex} />
-      <Tab.Screen name="Favorite" component={Favorite} />
-      <Tab.Screen name="Account" component={Account} />
+      <Tab.Screen
+        name="Podex"
+        component={Pokedex}
+        options={{
+          tabBarLabel: "Favoritos",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name="heart" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Favorite"
+        component={Favorite}
+        options={{
+          tabBarLabel: "",
+          tabBarIcon: () => renderPokeball(),
+        }}
+      />
+      <Tab.Screen
+        name="Account"
+        component={Account}
+        options={{
+          tabBarLabel: "Account",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name="user" size={24} color="black" />
+          ),
+        }}
+      />
     </Tab.Navigator>
+  );
+}
+
+function renderPokeball(color, size) {
+  return (
+    <Image
+      source={require("../assets/pokeball.png")}
+      style={{ width: 75, height: 75 }}
+    />
   );
 }
