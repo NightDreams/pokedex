08632a904fbd991b58a2eb24a79726ddda07e165
NightDreams <yayodcv@gmail.com>
2022-04-30 14:48:45 -0500

    C5 - Consume PokeApi with fetch in  Pokedex screen.

--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, SafeAreaView } from "react-native";
+import { getPokemonsApi } from "../api/pokemon";
 
 export default function Pokedex() {
+  useEffect(() => {
+    (async () => {
+      await loadPokemons();
+    })();
+  }, []);
+
+  const loadPokemons = async () => {
+    try {
+      const response = await getPokemonsApi();
+      console.log(
+        "🚀 ~ file: Pokedex.js ~ line 15 ~ loadPokemons ~ response",
+        response
+      );
+    } catch (error) {
+      console.error(
+        "🚀 ~ file: Pokedex.js ~ line 23 ~ loadPokemons ~ error",
+        error
+      );
+    }
+  };
+
   return (
     <SafeAreaView>
       <Text>Pokedex</Text>
---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+export const API_HOST = "https://pokeapi.co/api/v2";
+process.env.API_HOST = API_HOST;
